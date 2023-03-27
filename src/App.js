2123714c099b43a212109fffe7ec913ec4184961
Jorjikia Beka <jorjikiabeka@Jorjikias-MacBook-Air.local>
2023-03-27 16:38:44 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { useState } from "react";
 function App() {
   const [addTask, setAddTask] = useState([]);
   const [addValue, setAddValue] = useState("");
-  
+  const [changeClass, setChangeClass] = useState(false);
   const handleAddTask = () => {
     if (addValue.trim()) {
       setAddTask([...addTask, addValue.trim()]);
@@ -22,10 +22,15 @@ function App() {
   const handleInputChange = (event) => {
     setAddValue(event.target.value);
   };
- 
-const handleDeleteAll = () =>{
-  setAddTask([]);
-}
+
+  const handleDeleteAll = () => {
+    setAddTask([]);
+  };
+
+  const handleChangeClass = () =>{
+    setChangeClass(!changeClass);
+  }
+  
   return (
     <>
       <div className="container">
@@ -39,31 +44,42 @@ const handleDeleteAll = () =>{
             placeholder="add Todo..."
           />
           <span className="addButton">
-            <button className="add" onClick={handleAddTask}>+ </button>
-            <button className="clear" onClick={handleDeleteAll}>Clear</button>
+            <button className="add" onClick={handleAddTask}>
+              +{" "}
+            </button>
+            <button className="clear" onClick={handleDeleteAll}>
+              Clear
+            </button>
           </span>
         </div>
         <ul className="todoList">
           {addTask.map((task, index) => {
             return (
               <div className="oneItem" key={index}>
-                <li className="todoListInner">
-                <input className="check" type="checkbox" />
+                <li className={changeClass ? "listActive" : "todoListInner"} >
+                  <input onChange={handleChangeClass}className="check" type="checkbox" />
                   {task}
-                  
 
-                  <button className="delButton" onClick={() => handleDeleteTask(index)}>X</button>
-                  
-                  
+                  <button
+                    className="delButton"
+                    onClick={() => handleDeleteTask(index)}
+                  >
+                    X
+                  </button>
                 </li>
 
                 <img src={line} alt="" />
               </div>
             );
           })}
-           
         </ul>
-       
+      </div>
+      <div className="footer">
+        <div className="leftItems">{addTask.length} Items </div>
+        <div className="all">All </div>
+        <div className="active">active</div>
+        <div className="completed">completed</div>
+        <div className="clearCompleted">clear completed</div>
       </div>
     </>
   );
