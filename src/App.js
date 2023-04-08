5333039689338aa37c448def217e528e6bd1c444
Jorjikia Beka <jorjikiabeka@Jorjikias-MacBook-Air.local>
2023-04-08 12:29:13 +0400

    new

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from "react";
 import "./App.css";
-import line from "./images/Rectangle.png";
+
 import { useState } from "react";
 
 function App() {
   const [addTask, setAddTask] = useState([]);
   const [addValue, setAddValue] = useState("");
- 
+
   const handleAddTask = () => {
     if (addValue.trim()) {
-      setAddTask([...addTask,{task:addValue, active:true, id:Math.floor(Math.random(3 * 1000) * 1000)}]);
+      setAddTask([
+        ...addTask,
+        {
+          task: addValue,
+          active: true,
+          id: Math.floor(Math.random(3 * 1000) * 1000),
+        },
+      ]);
       setAddValue("");
     }
   };
-console.log(addTask);
+  console.log(addTask);
   const handleDeleteTask = (index) => {
     const newTaskList = addTask.filter((task, i) => i !== index);
     setAddTask(newTaskList);
@@ -24,19 +31,20 @@ console.log(addTask);
   };
 
   const handleDeleteAll = () => {
-    setAddTask([]); 
+    setAddTask([]);
   };
 
-const handleChange = (id) =>{
-  
-  setAddTask(addTask.map(todo =>{
-    if(todo.id === id){
-      todo.active = !todo.active
-    }
-    return todo;
-  }))
-}
-  
+  const handleChange = (id) => {
+    setAddTask(
+      addTask.map((todo) => {
+        if (todo.id === id) {
+          todo.active = !todo.active;
+        }
+        return todo;
+      })
+    );
+  };
+
   return (
     <>
       <div className="container">
@@ -44,7 +52,6 @@ const handleChange = (id) =>{
         <div className="input">
           <input
             className="addTodo"
-
             value={addValue}
             onChange={handleInputChange}
             type="text"
@@ -63,8 +70,16 @@ const handleChange = (id) =>{
           {addTask.map((task, index) => {
             return (
               <div className="oneItem" key={index}>
-                <li className={`${task.active === true ? "todoListInner" : "listActive"}`} >
-                  <input onClick={()=>handleChange(task.id)} className="check" type="checkbox" />
+                <li
+                  className={`${
+                    task.active === true ? "todoListInner" : "listActive"
+                  }`}
+                >
+                  <input
+                    onClick={() => handleChange(task.id)}
+                    className="check"
+                    type="checkbox"
+                  />
                   {task.task}
 
                   <button
@@ -74,19 +89,10 @@ const handleChange = (id) =>{
                     X
                   </button>
                 </li>
-
-                <img src={line} alt="" />
               </div>
             );
           })}
         </ul>
-      </div>
-      <div className="footer">
-        <div className="leftItems">{addTask.length} Items </div>
-        <div className="all">All </div>
-        <div className="active">active</div>
-        <div className="completed">completed</div>
-        <div className="clearCompleted">clear completed</div>
       </div>
     </>
   );
